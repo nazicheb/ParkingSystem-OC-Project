8f3b9eb03ae05046bc988fa9863e386ede3df414
naziha.chebil@lifen.fr <72403363+nazihachebil@users.noreply.github.com>
2021-02-24 16:26:10 +0100

    Fix integration tests

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,5 +1,6 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
@@ -13,7 +14,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
+
+import java.util.Date;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -52,6 +59,12 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
+        
+        String checktic=ticketDAO.getTicket("ABCDEF").getVehicleRegNumber();
+        Boolean parktab=ticketDAO.getTicket("ABCDEF").getParkingSpot().isAvailable();
+        
+        assertEquals("ABCDEF", checktic);
+        assertEquals(false, parktab);
     }
 
     @Test
@@ -60,6 +73,16 @@ public class ParkingDataBaseIT {
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processExitingVehicle();
         //TODO: check that the fare generated and out time are populated correctly in the database
+        
+        Double genrfare=ticketDAO.getTicket("ABCDEF").getPrice();
+        Date ouTime = ticketDAO.getTicket("ABCDEF").getOutTime();
+       
+        Boolean check=false;
+        
+        if (genrfare!=null && ouTime!=null) check=true;
+        assertEquals(true, check);
+        
+        
     }
 
 }
